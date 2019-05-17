cfe62c8ed78e75f41704255a4cec60a28def4036
nn <npr_0815@163.com>
2019-05-17 12:04:39 +0800

    finished practice04

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,54 @@
 package practice04;
 
 public class Person {
+    //属性
+    private String name;
+    private int age;
+
+    //无参构造方法
+    public Person() {
+    }
+
+    //构造方法
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //set、get方法
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * 调用basicIntroduce
+     *
+     * @return String
+     */
+    public String introduce() {
+        return basicIntroduce();
+    }
+
+    /**
+     * basicIntroduce方法返回一个字符串形如：
+     * My name is Tom. I am 21 years old.
+     *
+     * @return String
+     */
+    public String basicIntroduce(){
+        String introduce = "My name is " + this.name + ". I am " + this.age + " years old.";
+        return introduce;
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,14 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    //构造方法
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.basicIntroduce() + " I am a Worker. I have a job.";
+        return introduce;
+    }
 }
