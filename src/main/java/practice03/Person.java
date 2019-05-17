76136786f53a10ae6dc27ca35537acc781cdc932
nn <npr_0815@163.com>
2019-05-17 11:43:25 +0800

    finished practice03

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,45 @@
 package practice03;
 
 public class Person {
+    //属性
+    private String name;
+    private int age;
+
+    //无参构造方法
+    public Person() {
+    }
+
+    //构造方法
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //set、get方法
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * introduce方法返回一个字符串形如：
+     * My name is Tom. I am 21 years old.
+     *
+     * @return String
+     */
+    public String introduce() {
+        String introduce = "My name is " + this.name + ". I am " + this.age + " years old.";
+        return introduce;
+    }
 }
--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,30 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    //属性
+    private int klass;
+
+    //无参构造方法
+    public Student() {
+    }
+
+    //有参构造方法
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = "I am a Student. I am at Class " + this.klass + ".";
+        return introduce;
+    }
 }
