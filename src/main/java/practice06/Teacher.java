b6263b53edb0c235e8ccca59e3dc714a8c6ed9f2
nn <npr_0815@163.com>
2019-05-17 12:30:30 +0800

    finished practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,40 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    //属性
+    private int klass;
+
+    //无参构造方法
+    public Teacher() {
+    }
+
+    //有参构造方法
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    //set、get方法
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce;
+        if (this.getKlass() == 0) {
+            introduce = super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            introduce = super.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+        }
+        return introduce;
+    }
 }
