9b77a877e01772684d7585aee14637fe3c68aafc
nn <npr_0815@163.com>
2019-05-17 12:15:18 +0800

    finished practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,31 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    //属性
+    private int klass;
+
+    //无参构造方法
+    public Student() {
+    }
+
+    //有参构造方法
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    //set、get方法
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+        return introduce;
+    }
 }
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,14 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    //构造方法
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce() + " I am a Worker. I have a job.";
+        return introduce;
+    }
 }
